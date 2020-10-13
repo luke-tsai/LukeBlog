44ea289cbd127a66cc6c9ac159910d6b1bace606
luke-tsai <luke.s.tsai@gmail.com>
2020-10-12 21:00:01 -0400

    Switch template
    moved previous rev to subfolder

--- Blog.m
+++ Blog.m
@@ -2,62 +2,51 @@ function Blog
     clc
     
     %% imports tidbits
-    fileloc='C:\Users\ltsai\Documents\Personal\Github\LukeWebsite\LukeBlog\';
+    fileloc='C:\Users\ltsai\Documents\Personal\Github\LukeWebsite\LukeBlog\strata\';
        
-    source_files = dir(fullfile([fileloc,'Blog Posts\'], '*.html'));
-    initial=fileread([fileloc,'Formatting\1 Initial.txt']);
-    article=fileread([fileloc,'Formatting\2 Article.txt']);
-    space=fileread([fileloc,'Formatting\3 Space.txt']);
-    snippet=fileread([fileloc,'Formatting\4 Snippet.txt']);
-    ending=fileread([fileloc,'Formatting\5 Ending.txt']);
+    source_files = dir(fullfile([fileloc,'blogposts\'], '*.html'));
+    header=fileread([fileloc,'formatting\header.txt']);
+    footer=fileread([fileloc,'formatting\footer.txt']);
+    body=fileread([fileloc,'formatting\indexbody.txt']);
     output=[fileloc,'index.html'];
     
     fid = fopen(output,'w');
-    fprintf(fid, '%s', initial);
+    posthead=strrep(header,'[title]','Blog');
+    fprintf(fid, '%s', posthead);
     fclose(fid);
     
     fid = fopen(output,'a');
     for i = 1:length(source_files)
         clear data data2 posttitle posttags postid postlines
         disp([num2str(i),' of ',num2str(length(source_files))])
-        data=fileread([fileloc,'Blog Posts\',source_files(i).name]);
-        data2=(strsplit(data,{char(13),char(10)},'CollapseDelimiters',true));
-        posttitle=char(data2(1));
-        posttitle=posttitle(8:end);
-        postid=source_files(i).name(1:(length(source_files(i).name)-5));
-        posttext=strjoin(data2(4:end));
-        
-        post=article;
-        post=strrep(post,'[id]',postid);
-        post=strrep(post,'[title]',posttitle);
-        post=strrep(post,'[body]',posttext);
-        fprintf(fid, '%s', post);
-    end
-        
-    fprintf(fid, '%s', space);
-    
-    for i = 1:length(source_files)
-        clear data data2 posttitle posttags postid postlines
-        disp([num2str(i),' of ',num2str(length(source_files))])
-        data=fileread([fileloc,'Blog Posts\',source_files(i).name]);
+        data=fileread([fileloc,'blogposts\',source_files(i).name]);
         data2=(strsplit(data,{char(13),char(10)},'CollapseDelimiters',true));
         posttitle=char(data2(1));
         posttitle=posttitle(8:end);
         posttags=char(data2(2));
         posttags=posttags(7:end);
         postid=source_files(i).name(1:(length(source_files(i).name)-5));
-        posttext=char(data2(4));
-        posttext = regexprep(posttext,'<.*?>','');
-        
-        post=snippet;
-        post=strrep(post,'[id]',postid);
+        snippet = regexprep(char(data2(4)),'<.*?>','');
+
+        post=strrep(body,'[id]',postid);
         post=strrep(post,'[title]',posttitle);
         post=strrep(post,'[tag]',posttags);
-        post=strrep(post,'[body-snippet]',posttext);
+        post=strrep(post,'[body-snippet]',snippet);
         fprintf(fid, '%s', post);
+        
+        posthead=strrep(header,'[title]',posttitle);
+        fidpost = fopen([fileloc,source_files(i).name],'w');
+        fprintf(fidpost, '%s', posthead);
+        fprintf(fidpost, '%s', ['<h2>',posttitle,'</h2>']);
+        for j=4:length(data2)
+            fprintf(fidpost, '%s', char(data2(j)));
+        end
+        fprintf(fidpost, '%s', "<a href='index.html'>< Return to Blog</a>");
+        fprintf(fidpost, '%s', footer);
+        fclose(fidpost);
     end
     
-    fprintf(fid, '%s', ending);
+    fprintf(fid, '%s', footer);
     fclose(fid);
 
     disp("Done")
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,27 @@
 (function($) {
 
-	var	$window = $(window),
+	var $window = $(window),
 		$body = $('body'),
-		$wrapper = $('#wrapper'),
 		$header = $('#header'),
 		$footer = $('#footer'),
 		$main = $('#main'),
-		$main_articles = $main.children('article');
+		settings = {
+
+			// Parallax background effect?
+				parallax: true,
+
+			// Parallax factor (lower = more intense, higher = less intense).
+				parallaxFactor: 20
+
+		};
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
+			xlarge:  [ '1281px',  '1800px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ '481px',   '736px'  ],
+			xsmall:  [ null,      '480px'  ],
 		});
 
 	// Play initial animations on page load.
@@ -25,399 +31,151 @@
 			}, 100);
 		});
 
-	// Fix: Flexbox min-height bug on IE.
-		if (browser.name == 'ie') {
+	// Touch?
+		if (browser.mobile) {
 
-			var flexboxFixTimeoutId;
+			// Turn on touch mode.
+				$body.addClass('is-touch');
 
-			$window.on('resize.flexbox-fix', function() {
-
-				clearTimeout(flexboxFixTimeoutId);
-
-				flexboxFixTimeoutId = setTimeout(function() {
-
-					if ($wrapper.prop('scrollHeight') > $window.height())
-						$wrapper.css('height', 'auto');
-					else
-						$wrapper.css('height', '100vh');
-
-				}, 250);
-
-			}).triggerHandler('resize.flexbox-fix');
+			// Height fix (mostly for iOS).
+				window.setTimeout(function() {
+					$window.scrollTop($window.scrollTop() + 1);
+				}, 0);
 
 		}
 
-	// Nav.
-		var $nav = $header.children('nav'),
-			$nav_li = $nav.find('li');
+	// Footer.
+		breakpoints.on('<=medium', function() {
+			$footer.insertAfter($main);
+		});
 
-		/*
-		// Add "middle" alignment classes if we're dealing with an even number of items.
-			if ($nav_li.length % 2 == 0) {
+		breakpoints.on('>medium', function() {
+			$footer.appendTo($header);
+		});
 
-				$nav.addClass('use-middle');
-				$nav_li.eq( ($nav_li.length / 2)).addClass('is-middle');
+	// Header.
 
-			}
-		*/
-		
-	// Main.
-		var	delay = 325,
-			locked = false;
+		// Parallax background.
 
-		// Methods.
-			$main._show = function(id, initial) {
+			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+				if (browser.name == 'ie'
+				||	browser.mobile)
+					settings.parallax = false;
 
-				var $article = $main_articles.filter('#' + id);
+			if (settings.parallax) {
 
-				// No such article? Bail.
-					if ($article.length == 0)
-						return;
+				breakpoints.on('<=medium', function() {
 
-				// Handle lock.
+					$window.off('scroll.strata_parallax');
+					$header.css('background-position', '');
 
-					// Already locked? Speed through "show" steps w/o delays.
-						if (locked || (typeof initial != 'undefined' && initial === true)) {
+				});
 
-							// Mark as switching.
-								$body.addClass('is-switching');
+				breakpoints.on('>medium', function() {
 
-							// Mark as visible.
-								$body.addClass('is-article-visible');
+					$header.css('background-position', 'left 0px');
 
-							// Deactivate all articles (just in case one's already active).
-								$main_articles.removeClass('active');
-
-							// Hide header, footer.
-								$header.hide();
-								$footer.hide();
-
-							// Show main, article.
-								$main.show();
-								$article.show();
-
-							// Activate article.
-								$article.addClass('active');
-
-							// Unlock.
-								locked = false;
-
-							// Unmark as switching.
-								setTimeout(function() {
-									$body.removeClass('is-switching');
-								}, (initial ? 1000 : 0));
-
-							return;
-
-						}
-
-					// Lock.
-						locked = true;
-
-				// Article already visible? Just swap articles.
-					if ($body.hasClass('is-article-visible')) {
-
-						// Deactivate current article.
-							var $currentArticle = $main_articles.filter('.active');
-
-							$currentArticle.removeClass('active');
-
-						// Show article.
-							setTimeout(function() {
-
-								// Hide current article.
-									$currentArticle.hide();
-
-								// Show article.
-									$article.show();
-
-								// Activate article.
-									setTimeout(function() {
-
-										$article.addClass('active');
-
-										// Window stuff.
-											$window
-												.scrollTop(0)
-												.triggerHandler('resize.flexbox-fix');
-
-										// Unlock.
-											setTimeout(function() {
-												locked = false;
-											}, delay);
-
-									}, 25);
-
-							}, delay);
-
-					}
-
-				// Otherwise, handle as normal.
-					else {
-
-						// Mark as visible.
-							$body
-								.addClass('is-article-visible');
-
-						// Show article.
-							setTimeout(function() {
-
-								// Hide header, footer.
-									$header.hide();
-									$footer.hide();
-
-								// Show main, article.
-									$main.show();
-									$article.show();
-
-								// Activate article.
-									setTimeout(function() {
-
-										$article.addClass('active');
-
-										// Window stuff.
-											$window
-												.scrollTop(0)
-												.triggerHandler('resize.flexbox-fix');
-
-										// Unlock.
-											setTimeout(function() {
-												locked = false;
-											}, delay);
-
-									}, 25);
-
-							}, delay);
-
-					}
-
-			};
-
-			$main._hide = function(addState) {
-
-				var $article = $main_articles.filter('.active');
-
-				// Article not visible? Bail.
-					if (!$body.hasClass('is-article-visible'))
-						return;
-
-				// Add state?
-					if (typeof addState != 'undefined'
-					&&	addState === true)
-						history.pushState(null, null, '#');
-
-				// Handle lock.
-
-					// Already locked? Speed through "hide" steps w/o delays.
-						if (locked) {
-
-							// Mark as switching.
-								$body.addClass('is-switching');
-
-							// Deactivate article.
-								$article.removeClass('active');
-
-							// Hide article, main.
-								$article.hide();
-								$main.hide();
-
-							// Show footer, header.
-								$footer.show();
-								$header.show();
-
-							// Unmark as visible.
-								$body.removeClass('is-article-visible');
-
-							// Unlock.
-								locked = false;
-
-							// Unmark as switching.
-								$body.removeClass('is-switching');
-
-							// Window stuff.
-								$window
-									.scrollTop(0)
-									.triggerHandler('resize.flexbox-fix');
-
-							return;
-
-						}
-
-					// Lock.
-						locked = true;
-
-				// Deactivate article.
-					$article.removeClass('active');
-
-				// Hide article.
-					setTimeout(function() {
-
-						// Hide article, main.
-							$article.hide();
-							$main.hide();
-
-						// Show footer, header.
-							$footer.show();
-							$header.show();
-
-						// Unmark as visible.
-							setTimeout(function() {
-
-								$body.removeClass('is-article-visible');
-
-								// Window stuff.
-									$window
-										.scrollTop(0)
-										.triggerHandler('resize.flexbox-fix');
-
-								// Unlock.
-									setTimeout(function() {
-										locked = false;
-									}, delay);
-
-							}, 25);
-
-					}, delay);
-
-
-			};
-
-		// Articles.
-			$main_articles.each(function() {
-
-				var $this = $(this);
-
-				// Close.
-					$('<div class="close">Close</div>')
-						.appendTo($this)
-						.on('click', function() {
-							location.hash = '';
-						});
-
-				// Prevent clicks from inside article from bubbling.
-					$this.on('click', function(event) {
-						event.stopPropagation();
+					$window.on('scroll.strata_parallax', function() {
+						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
 					});
 
-			});
+				});
 
-		// Events.
-			$body.on('click', function(event) {
+				$window.on('load', function() {
+					$window.triggerHandler('scroll');
+				});
 
-				// Article visible? Hide.
-					if ($body.hasClass('is-article-visible')){
-					    if (location.hash.startsWith("#proj")
-					    &&  history.length > 2 ) {
-						    window.history.back();
-						} else{
-							$main._hide(true);
-						}
-				    }
-			});
+			}
 
-			$window.on('keyup', function(event) {
-
-				switch (event.keyCode) {
-
-					case 27:
-
-						// Article visible? Hide.
-							if ($body.hasClass('is-article-visible'))
-								$main._hide(true);
-
-						break;
-
-					default:
-						break;
-
-				}
-
-			});
-
+	// Main Sections: Two.
+	// Hashchange
 			$window.on('hashchange', function(event) {
 
-				// Empty hash?
-					if (location.hash == ''
-					||	location.hash == '#') {
+				switch(location.hash){
+            	case '#': case '':
+            	 	// Prevent default.
+						event.preventDefault();
+						event.stopPropagation();
+					// Hide.
+						$main._hide();
+            	 	$(".theology").show();
+            	 	$(".philosophy").show();
+            	 	$(".faith").show();
+            	 	$(".art").show();
+            	 	$(".writing").show();
+            	 	break;
+            	case "#theology":
+            	 	$(".theology").hide();
+            	 	$(".philosophy").hide();
+            	 	$(".faith").hide();
+            	 	$(".art").hide();
+            	 	$(".writing").hide();
+            	 	
+            	 	$(".theology").show();
+            	 	break;
+            	case "#philosophy":
+            	 	$(".theology").hide();
+            	 	$(".philosophy").hide();
+            	 	$(".faith").hide();
+            	 	$(".art").hide();
+            	 	$(".writing").hide();
+            	 	
+            	 	$(".philosophy").show();
+            	 	break;
+            	case "#faith":
+            	 	$(".theology").hide();
+            	 	$(".philosophy").hide();
+            	 	$(".faith").hide();
+            	 	$(".art").hide();
+            	 	$(".writing").hide();
+            	 	
+            	 	$(".faith").show();
+            	 	break;
+            	case "#art":
+            	 	$(".theology").hide();
+            	 	$(".philosophy").hide();
+            	 	$(".faith").hide();
+            	 	$(".art").hide();
+            	 	$(".writing").hide();
+            	 	
+            	 	$(".art").show();
+            	 	break;
+            	case "#writing":
+            	 	$(".theology").hide();
+            	 	$(".philosophy").hide();
+            	 	$(".faith").hide();
+            	 	$(".art").hide();
+            	 	$(".writing").hide();
+            	 	
+            	 	$(".writing").show();
+            	 	break;
+            	default:
+            	 	// Prevent default.
+						event.preventDefault();
+						event.stopPropagation();
 
-						// Prevent default.
-							event.preventDefault();
-							event.stopPropagation();
-
-						// Hide.
-							$main._hide();
-
-					}
-
-				// Otherwise, check for a matching article.
-					else if ($main_articles.filter(location.hash).length > 0) {
-
-						// Prevent default.
-							event.preventDefault();
-							event.stopPropagation();
-
-						// Show article.
-							$main._show(location.hash.substr(1));
-
-					}
-
-			});
-
-		// Scroll restoration.
-		// This prevents the page from scrolling back to the top on a hashchange.
-			if ('scrollRestoration' in history)
-				history.scrollRestoration = 'manual';
-			else {
-
-				var	oldScrollPos = 0,
-					scrollPos = 0,
-					$htmlbody = $('html,body');
-
-				$window
-					.on('scroll', function() {
-
-						oldScrollPos = scrollPos;
-						scrollPos = $htmlbody.scrollTop();
-
-					})
-					.on('hashchange', function() {
-						$window.scrollTop(oldScrollPos);
-					});
-
-			}
-
-
-            $("#toggler").click(function(){
-                if ($(".technical").is(":visible")){
-                    $(".technical").hide(200);
-                    location.hash = '#personal';
-                    $(".personal").delay(200).show(200);
-                } else{
-                    $(".personal").hide(200);
-                    location.hash = '#technical';
-                    $(".technical").delay(200).show(200);
-                    }
-                });
-
-		// Initialize.
-
-			// Hide main, articles.
-				$main.hide();
-				$main_articles.hide();
-
-            if (location.hash.startsWith("#personal")) {
-                    document.getElementById("toggler").checked = true;
-                    $(".technical").hide();
-            } else{
-                    document.getElementById("toggler").checked = false;
-                    $(".personal").hide();
+					// Show article.
+						$main._show(location.hash.substr(1));
             }
 
-			// Initial article.
-				if (location.hash != ''
-				&&	location.hash != '#')
-					$window.on('load', function() {
-						$main._show(location.hash.substr(1), true);
-					});
+			});
+		// Lightbox gallery.
+			$window.on('load', function() {
+
+				$('#two').poptrox({
+					caption: function($a) { return $a.next('h3').text(); },
+					overlayColor: '#2c2c2c',
+					overlayOpacity: 0.85,
+					popupCloserText: '',
+					popupLoaderText: '',
+					selector: '.work-item a.image',
+					usePopupCaption: true,
+					usePopupDefaultStyling: false,
+					usePopupEasyClose: false,
+					usePopupNav: true,
+					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+				});
+
+			});
 
 })(jQuery);
